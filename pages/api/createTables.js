c75b9c4972b5ef47d90a2a14ce5ed7b4baeae177
vastlaan <vastlaan@mail.com>
2021-03-15 16:59:58 +0100

    deactivate tables creation

--- pages/api/createTables.js
+++ pages/api/createTables.js
@@ -2,13 +2,13 @@ import Connection from "../../database";
 
 export default async (req, res) => {
     const db = new Connection();
-    const resultOfCreatingUsersTable = await db.createTableUsers();
-    const resultOfCreatingPostsTable = await db.createTablePosts();
-    const resultOfCreatingRecipesTable = await db.createTableRecipes();
-    const resultOfCreatingEventsTable = await db.createTableEvents();
-    const resultOfCreatingMembersTable = await db.createTableMembers();
-    const resultOfCreatingSchedulesTable = await db.createTableSchedules();
-    const resultOfCreatingPreparationsTable = await db.createTablePreparations();
+    // const resultOfCreatingUsersTable = await db.createTableUsers();
+    // const resultOfCreatingPostsTable = await db.createTablePosts();
+    // const resultOfCreatingRecipesTable = await db.createTableRecipes();
+    // const resultOfCreatingEventsTable = await db.createTableEvents();
+    // const resultOfCreatingMembersTable = await db.createTableMembers();
+    // const resultOfCreatingSchedulesTable = await db.createTableSchedules();
+    // const resultOfCreatingPreparationsTable = await db.createTablePreparations();
 
     res.status(200).json({ message: "Success" });
     db.getDatabase().destroy();
