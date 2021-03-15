a5485daca684f59dc719d3bed647ccefea98db1b
vastlaan <vastlaan@mail.com>
2021-03-15 16:52:52 +0100

    activate tables creation

--- pages/api/createTables.js
+++ pages/api/createTables.js
@@ -2,16 +2,14 @@ import Connection from "../../database";
 
 export default async (req, res) => {
     const db = new Connection();
-    //const resultOfCreatingUsersTable = await  db.createTableUsers();
-    // const resultOfCreatingPostsTable = await  db.createTablePosts();
-    // const resultOfCreatingRecipesTable = await  db.createTableRecipes();
-    // const resultOfCreatingEventsTable = await db.createTableEvents();
-    // const resultOfCreatingMembersTable = await db.createTableMembers();
-    // const resultOfCreatingSchedulesTable = await db.createTableSchedules();
-    // const resultOfCreatingPreparationsTable = await db.createTablePreparations();
+    const resultOfCreatingUsersTable = await db.createTableUsers();
+    const resultOfCreatingPostsTable = await db.createTablePosts();
+    const resultOfCreatingRecipesTable = await db.createTableRecipes();
+    const resultOfCreatingEventsTable = await db.createTableEvents();
+    const resultOfCreatingMembersTable = await db.createTableMembers();
+    const resultOfCreatingSchedulesTable = await db.createTableSchedules();
+    const resultOfCreatingPreparationsTable = await db.createTablePreparations();
 
-    //const users = await db.select("*").from("users");
-
-    res.status(200).json({ resultOfCreatingPreparationsTable });
+    res.status(200).json({ message: "Success" });
     db.getDatabase().destroy();
 };
