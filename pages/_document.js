cc5f18c593ee24ab9685af8e0ef9630b61290faf
vastlaan <vastlaan@mail.com>
2021-03-18 16:20:10 +0100

    Add gtag

--- pages/_document.js
+++ pages/_document.js
@@ -41,9 +41,9 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600&family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
                         rel="stylesheet"
                     />
-                    {/* <script
+                    <script
                         async
-                        src="https://www.googletagmanager.com/gtag/js?id=UA-132849357-5"
+                        src="https://www.googletagmanager.com/gtag/js?id=UA-132849357-3"
                     />
 
                     <script
@@ -52,12 +52,12 @@ class MyDocument extends Document {
                                 window.dataLayer = window.dataLayer || [];
                                 function gtag(){dataLayer.push(arguments);}
                                 gtag('js', new Date());
-                                gtag('config', 'UA-132849357-5', {
+                                gtag('config', 'UA-132849357-3', {
                                     page_path: window.location.pathname,
                                 });
                             `,
                         }}
-                    /> */}
+                    />
                 </Head>
                 <body>
                     <Main />
